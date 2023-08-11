126995dafa4ee55efad43e8f8c16fed7cea614e7
mohid2 <fukahamaroc@gmail.com>
2023-08-11 18:10:53 +0200

    creando la interfaz service de usuario

---
+++ src/main/java/com/ejemplo/demopaginaweb/service/IUsuarioService.java
@@ -0,0 +1,7 @@
+package com.ejemplo.demopaginaweb.service;
+
+import com.ejemplo.demopaginaweb.entity.Usuario;
+
+public interface IUsuarioService {
+    Usuario createUser(Usuario usuario) throws Exception;
+}
