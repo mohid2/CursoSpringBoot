d80854daf9b95c4c22dadfbdce5dcb3bcfd4e586
mohid2 <fukahamaroc@gmail.com>
2023-08-11 17:39:16 +0200

    creando un layout para la pagina

---
+++ src/main/resources/import.sql
@@ -0,0 +1,103 @@
+-- Insertar clientes de ejemplo
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('John Doe', 'Doe', 'johndoe@example.com', '2023-01-01',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Jane Smith', 'Smith', 'janesmith@example.com', '2023-02-15',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Robert Johnson', 'Johnson', 'robertjohnson@example.com', '2023-03-10',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Michael Brown', 'Brown', 'michaelbrown@example.com', '2023-04-05',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Jennifer Davis', 'Davis', 'jenniferdavis@example.com', '2023-05-20',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('William Wilson', 'Wilson', 'williamwilson@example.com', '2023-06-12',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Jessica Anderson', 'Anderson', 'jessicaanderson@example.com', '2023-07-03',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('David Thomas', 'Thomas', 'davidthomas@example.com', '2023-08-18',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Linda Martinez', 'Martinez', 'lindamartinez@example.com', '2023-09-25',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Daniel Taylor', 'Taylor', 'danieltaylor@example.com', '2023-10-11',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Sarah Garcia', 'Garcia', 'sarahgarcia@example.com', '2023-11-30',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('James Hernandez', 'Hernandez', 'jameshernandez@example.com', '2023-12-22',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Karen Lopez', 'Lopez', 'karenlopez@example.com', '2024-01-14',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Charles Adams', 'Adams', 'charlesadams@example.com', '2024-02-07',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Amanda Lewis', 'Lewis', 'amandalewis@example.com', '2024-03-19',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Matthew Lee', 'Lee', 'matthewlee@example.com', '2024-04-30',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Christopher Clark', 'Clark', 'christopherclark@example.com', '2024-06-28',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Emily Hall', 'Hall', 'emilyhall@example.com', '2024-05-15',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Maria Wright', 'Wright', 'mariawright@example.com', '2024-07-09',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Joshua Turner', 'Turner', 'joshuaturner@example.com', '2024-08-14',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Nicole Scott', 'Scott', 'nicolescott@example.com', '2024-09-02',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Andrew Green', 'Green', 'andrewgreen@example.com', '2024-10-18',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Melissa Martinez', 'Martinez', 'melissamartinez@example.com', '2024-11-25',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Benjamin Perez', 'Perez', 'benjaminperez@example.com', '2024-12-10',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Olivia Adams', 'Adams', 'oliviaadams@example.com', '2025-01-08',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Daniel Garcia', 'Garcia', 'danielgarcia@example.com', '2025-02-17',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Sophia Hernandez', 'Hernandez', 'sophiahernandez@example.com', '2025-03-22',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Ethan Johnson', 'Johnson', 'ethanjohnson@example.com', '2025-04-14',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Isabella Brown', 'Brown', 'isabellabrown@example.com', '2025-05-27',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Alexander Davis', 'Davis', 'alexanderdavis@example.com', '2025-06-09',"");
+INSERT INTO clientes (nombre, apellidos, email, fecha_alta, foto) VALUES ('Mia Wilson', 'Wilson', 'miawilson@example.com', '2025-07-19',"");
+
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('iPhone 13', 999.00, '2023-05-15');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Samsung Galaxy S22', 899.99, '2023-05-16');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Sony PlayStation 5', 499.00, '2023-05-14');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('LG 55-inch OLED TV', 1299.99, '2023-05-19');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Dell XPS 15 Laptop', 1199.00, '2023-05-13');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Bose QuietComfort Earbuds', 199.00, '2023-05-22');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Fitbit Charge 5', 179.95, '2023-05-23');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('GoPro HERO10 Black', 399.99, '2023-05-24');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Amazon Kindle Paperwhite', 129.99, '2023-05-25');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Microsoft Surface Pro 8', 1199.00, '2023-05-26');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Sony WH-1000XM4 Headphones', 349.99, '2023-05-27');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('LG Gram 17 Laptop', 1499.99, '2023-05-28');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Google Pixel 6 Pro', 899.00, '2023-05-29');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('OnePlus 9T', 699.00, '2023-05-30');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Nikon Z9 Camera', 5499.95, '2023-05-31');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Sony A90J OLED TV', 2999.99, '2023-06-01');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Bose SoundLink Revolve II', 179.00, '2023-06-02');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Apple iPad Pro 12.9', 1099.00, '2023-06-03');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Samsung Odyssey G7 Monitor', 799.99, '2023-06-04');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Huawei MateBook X Pro', 1499.00, '2023-06-05');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Fitbit Versa 3', 229.95, '2023-06-06');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Xbox Series X', 499.99, '2023-06-07');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Samsung Galaxy Watch 4', 249.00, '2023-06-08');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Apple AirPods Pro', 249.00, '2023-06-09');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Nintendo Switch Pro', 399.99, '2023-06-10');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Sony WF-1000XM4 Earbuds', 249.99, '2023-06-11');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Apple Watch Series 7', 349.00, '2023-05-18');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Xiaomi Mi 11 Lite', 249.50, '2023-05-17');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Canon EOS R6 Camera', 2499.95, '2023-05-21');
+INSERT INTO productos (nombre, precio, fecha_creacion) VALUES('Nintendo Switch OLED', 349.99, '2023-05-20');
+
+-- Insertar facturas relacionadas con clientes y productos existentes
+-- Aquí asumimos que la relación cliente-producto se establece mediante los primeros registros de clientes y productos que has proporcionado
+
+-- Factura 1 relacionada con Cliente 1 y Productos 1, 2, y 3
+INSERT INTO facturas (descripcion, observacion, fecha, cliente_id) VALUES('Factura 1', 'Observación Factura 1', '2023-05-15', 1);
+
+INSERT INTO item_facturas (cantidad, factura_id, producto_id) VALUES(2,1, 1);
+INSERT INTO item_facturas (cantidad, factura_id,producto_id) VALUES(3,1, 2);
+INSERT INTO item_facturas (cantidad, factura_id,producto_id) VALUES(1,1, 3);
+
+
+-- Factura 2 relacionada con Cliente 2 y Productos 4, 5, y 6
+INSERT INTO facturas (descripcion, observacion, fecha, cliente_id) VALUES('Factura 2', 'Observación Factura 2', '2023-05-16', 2);
+
+INSERT INTO item_facturas (cantidad,factura_id producto_id) VALUES(4, 2, 4);
+INSERT INTO item_facturas (cantidad,factura_id producto_id) VALUES(2, 2, 5);
+INSERT INTO item_facturas (cantidad,factura_id, producto_id) VALUES(3, 2, 6);
+
+
+-- Factura 3 relacionada con Cliente 3 y Productos 7, 8, y 9
+INSERT INTO facturas (descripcion, observacion, fecha, cliente_id) VALUES('Factura 3', 'Observación Factura 3', '2023-05-14', 3);
+
+INSERT INTO item_facturas (cantidad, factura_id, producto_id) VALUES(1,3, 7);
+INSERT INTO item_facturas (cantidad, factura_id, producto_id) VALUES(2,3, 8);
+INSERT INTO item_facturas (cantidad, factura_id, producto_id) VALUES(3,3, 9);
+
+-- Continuar con más facturas relacionadas con clientes y productos existentes..
+
+-- Insertar datos en la tabla usuarios
+
+INSERT INTO usuarios (nombre_usuario, password, activo) VALUES ('admin', 'moha', true);
+INSERT INTO usuarios (nombre_usuario, password, activo) VALUES('usuario','moha', true);
+
+-- Insertar datos en la tabla roles
+INSERT INTO roles (role, usuario_id) VALUES ('ROLE_USER', 1); -- Rol 'admin' pertenece a usuario con ID 1
+INSERT INTO roles (role, usuario_id) VALUES ('ROLE_ADMIN', 1); -- Rol 'usuario' pertenece a usuario con ID 1
+INSERT INTO roles (role, usuario_id) VALUES ('ROLE_USER', 2); -- Rol 'usuario' pertenece a usuario con ID 2
+
+
